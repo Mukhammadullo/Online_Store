c15bce21e0a1391b245d97ccdf5fa169f3a93f28
Muhammadullo <mnastulloev@gmail.com>
2023-12-06 15:03:50 +0500

    btn_style

--- page3.js
+++ page3.js
@@ -1,10 +1,7 @@
 let next = document.querySelector(".next")
 let prev = document.querySelector(".prev")
 
-let btnSee = document.querySelector(".btnSee")
-btnSee.onclick = () => {
-    window.location.replace = "login.html"
-}
+
 
 next.addEventListener('click', function () {
     let items = document.querySelectorAll(".item")
