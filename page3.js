d65d491eaebd0204c10fad45e37a849dd989f74d
Muhammadullo <mnastulloev@gmail.com>
2023-12-06 14:31:26 +0500

    cards style

--- page3.js
+++ page3.js
@@ -1,18 +1,21 @@
-let next=document.querySelector(".next")
-let prev=document.querySelector(".prev")
+let next = document.querySelector(".next")
+let prev = document.querySelector(".prev")
 
-next.addEventListener('click', function()
-{
-    let items=document.querySelectorAll(".item")
+let btnSee = document.querySelector(".btnSee")
+btnSee.onclick = () => {
+    window.location.replace = "login.html"
+}
+
+next.addEventListener('click', function () {
+    let items = document.querySelectorAll(".item")
     document.querySelector('.slide').appendChild(items[0])
 }
 )
 
 
 
-prev.addEventListener('click', function()
-{
-    let items=document.querySelectorAll(".item")
-    document.querySelector('.slide').prepend(items[items.length-1])
+prev.addEventListener('click', function () {
+    let items = document.querySelectorAll(".item")
+    document.querySelector('.slide').prepend(items[items.length - 1])
 }
 )
